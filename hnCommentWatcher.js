4fe27cc1d9ddb0efc4f748d018c80a854fa7c438
Christopher de Beer <christopherdebeer@gmail.com>
2011-12-29 09:43:06 +0000

    added comment diff count

--- hnCommentWatcher.js
+++ hnCommentWatcher.js
@@ -128,8 +128,10 @@ var Jenkins = {
 
 
 var comments = {};
+var commentCount = 0
 
 var init = function () {
+    commentCount = getCommentCount;
     $(".default").each(function (comment) {
         var text = $(".comment", this).text();
         var hashObj = Jenkins.hashlittle2(text,1);
@@ -141,6 +143,10 @@ var init = function () {
     setTimeout(loop, 5000);
 }
 
+var getCommentCount = function(){
+    return parseInt($(".subtext:first a:nth-child(4n)").text().replace(" comments",""));
+}
+
 var loop = function () {
     
     var thisUrl = document.location.href;
@@ -150,8 +156,12 @@ var loop = function () {
         $("body").html(data);
 
         // check whats new
-        var count = 0;
-        var num = $(".default").length;
+        $("#commDiff").remove();
+        var commentNum = getCommentCount();
+        if (commentNum > commentCount) {
+            var diff = commentNum - commentCount;
+            $(".subtext:first a:nth-child(4n)").prepend("<span id='commDiff'>(+" + diff.toString() + ")</span>");
+        }
         var first = true;
         $(".default").each(function (comment) {
             
@@ -182,8 +192,6 @@ var loop = function () {
 
                 if (first) {$.scrollTo($(this), 1000); first = false;}
             }
-
-            if (count >= num) console.log("Recieved " + count.toString() + " new comments.");
 
         })
     });
